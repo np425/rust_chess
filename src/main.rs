5c44af48b77bd1fe3a9f86c20f6d0fff00f5c829
np <nerpoc42@gmail.com>
2023-04-29 11:03:40 +0300

    Implemented pieces

--- src/main.rs
+++ src/main.rs
@@ -32,19 +32,79 @@ enum Piece {
 }
 
 impl Piece {
-    fn can_move(&self, board: &[Option<Piece>; 64], path: &Path) -> bool {
+    fn is_path_clear(board: &[Option<Piece>; 64], path: &Path) -> bool {
         let d = path.diff();
 
+        if d.x.abs() != d.y.abs() {
+            return false;
+        }
+        if d.x == 0 {
+            return true;
+        }
+
+        let incr = Coord {
+            x: if d.x > 0 { 1 } else { -1 },
+            y: if d.y > 0 { 1 } else { -1 },
+        };
+
+        let mut iter = Coord { x: path.from.x + incr.x, y: path.from.x + incr.y };
+
+        while path.from.x != path.to.x && path.from.y != path.to.y {
+            iter.x += incr.x;
+            iter.y += incr.y;
+
+            match board.get(iter.y * 8 + iter.x) {
+                Some(None) => true,
+                _ => return false
+            }
+        }
+
+        true
+    }
+
+    fn can_move(&self, board: &[Option<Piece>; 64], path: &Path) -> bool {
+        let d = path.diff();
+        if !Self::is_path_clear(board, path) {
+            return false
+        }
+
         match self {
-            Piece::Rook(player) => d.x == 0 && d.y != 0 || d.x != 0 && d.y == 0,
-            Piece::Knight(player) => {}
-            Piece::Bishop(player) => {}
-            Piece::Queen(player) => {}
-            Piece::King(player) => {}
-            Piece::Pawn(player) => {}
+            Piece::Rook(_) => {
+                d.x == 0 && d.y != 0 || d.x != 0 && d.y == 0
+            }
+            Piece::Bishop(_) => {
+                d.x.abs() == d.y.abs() && d.x != 0
+            }
+            Piece::Queen(_) => {
+                (d.x == 0 && d.y != 0 || d.x != 0 && d.y == 0) || (d.x.abs() == d.y.abs() && d.x != 0)
+            }
+            Piece::Knight(_) => {
+                d.x.abs() * d.y.abs() == 2
+            }
+            Piece::King(_) => {
+                // TODO: implement checks, etc
+                true
+            }
+            Piece::Pawn(player) => {
+                let second_rank_y = match player {
+                    Player::White => 1,
+                    Player::Black => 6,
+                };
+
+                let dir = match player {
+                    Player::White => 1,
+                    Player::Black => -1,
+                };
+
+                d.y == 1 * dir || second_rank_y == path.from.y && d.y == 2 * dir
+                // TODO: capturing
+                // TODO: en passant
+            }
         }
     }
 }
+
+// TODO: FEN, PGN, Game Impl
 
 struct Position {
     board: [Option<Piece>; 64],
