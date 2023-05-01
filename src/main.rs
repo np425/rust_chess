07a6e71ad127f0b767417382ed51a97a28b5a645
np <nerpoc42@gmail.com>
2023-05-01 10:38:50 +0300

    Refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,6 @@
-#[derive(Clone, Copy)]
-struct Coord {
-    x: usize,
-    y: usize,
-}
+// TODO: Split into separate files
 
-struct Path {
-    from: Coord,
-    to: Coord,
-}
-
-impl Path {
-    fn diff(&self) -> Coord {
-        Coord {
-            x: self.to.x - self.from.x,
-            y: self.to.y - self.from.y,
-        }
-    }
-
-    // TODO: take position, to not hardcore board size
-    fn is_clear(&self, board: &[Square; 64]) -> bool {
-        let d = self.diff();
-
-        if d.x.abs() != d.y.abs() {
-            return false;
-        }
-        if d.x == 0 {
-            return true;
-        }
-
-        let incr = Coord {
-            x: if d.x > 0 { 1 } else { -1 },
-            y: if d.y > 0 { 1 } else { -1 },
-        };
-
-        let mut iter = Coord { x: path.from.x + incr.x, y: path.from.x + incr.y };
-
-        while path.from.x != path.to.x && path.from.y != path.to.y {
-            iter.x += incr.x;
-            iter.y += incr.y;
-
-            match board.get(iter.y * 8 + iter.x) {
-                Some(Square::Empty) => true,
-                _ => return false
-            }
-        }
-
-        true
-    }
-}
-
-
+#[derive(Copy, Clone)]
 enum Player {
     White,
     Black,
@@ -75,27 +26,54 @@ enum Square {
 }
 
 impl Square {
-    fn player_ref(&self) -> Option<&Player> {
+    fn is_path_clear(&self, board: &Board, path: Path) -> bool {
+        let d = path.diff();
+
+        if d.x.abs() != d.y.abs() {
+            return false;
+        }
+        if d.x == 0 {
+            return true;
+        }
+
+        let incr = (
+            if d.x > 0 { 1 } else { -1 },
+            if d.y > 0 { 1 } else { -1 },
+        );
+
+        let iter = board.iter_path(path.from, incr);
+
+        for square in iter.take_while(|(coord, _)| coord != path.to) {
+            if let Some(Square::Empty) = square {
+                return false
+            }
+        }
+
+        true
+    }
+
+    fn player(&self) -> Option<Player> {
         match self {
-            Square::Knight(player) => Some(player),
-            Square::Bishop(player) => Some(player),
-            Square::Queen(player) => Some(player),
-            Square::Rook(player) => Some(player),
-            Square::King(player) => Some(player),
-            Square::Pawn(player) => Some(player),
+            Square::Knight(player) => Some(*player),
+            Square::Bishop(player) => Some(*player),
+            Square::Queen(player) => Some(*player),
+            Square::Rook(player) => Some(*player),
+            Square::King(player) => Some(*player),
+            Square::Pawn(player) => Some(*player),
             Square::Empty => None,
         }
     }
 
-    fn can_move(&self, pos: &Position, path: &Path) -> bool {
+    fn can_move(&self, pos: &Position, path: Path) -> bool {
         if !path.is_clear(&pos.board) {
             return false;
         }
 
         let d = path.diff();
 
-        // TODO: checks for current player
-        let no_checks = pos.checks.is_empty();
+        let player = self.player_ref()?;
+
+        let no_checks = pos.checks(player)?.is_empty();
 
         match self {
             // TODO: defending with pieces
@@ -113,7 +91,7 @@ impl Square {
             }
             Square::King(_) => {
                 // TODO: implement checks, etc
-                d.x.abs() <= 1 && d.y.abs() <= 1
+                d.x.abs() <= 1 && d.y.abs() <= 1 && !pos.is_coord_defended(path.to, player.enemy())
             }
             Square::Pawn(_) => {
                 let second_rank_y = match player {
@@ -135,32 +113,109 @@ impl Square {
     }
 }
 
-// TODO: FEN, PGN, Game Impl
 
 struct Position {
-    board: [Square; 64],
+    board: Board,
     king_pos: [Coord; 2],
     checks: Vec<Coord>,
     player: Player,
 }
 
 impl Position {
-    fn checks(&self, player: &Player) -> Option<&Vec<Coord>> {
+    fn checks(&self, player: Player) -> Option<&Vec<Coord>> {
         (player == self.player).then_some(&self.checks)
     }
 
-    fn is_square_defended(&self, coord: &Coord, by_player: Player) -> bool {
-        for (idx, square) in self.board.iter().enumerate() {
-            // TODO: refactor
+    // TODO: Maybe extract it as a standalone function
+    fn is_coord_defended(&self, coord: Coord, by_player: Player) -> bool {
+        for (target, square) in self.board.iter() {
+            let path = Path { from: coord, to: target };
+
             // TODO: can attack
-            if *square.player_ref() == by_player && square.can_move(self, &Path { from: Coord { x: idx % 8, y: idx / 8 }, to: *coord }) {
-                return false;
+            if *square.player_ref() == by_player && square.can_move(self, path) {
+                return true
             }
         }
         false
     }
 }
 
+struct Board {
+    squares: [Square; 64],
+}
+
+impl Board {
+    fn resolve_index(coord: Coord) -> usize {
+        coord.y * 8 + coord.x
+    }
+
+    pub fn get(&self, coord: Coord) -> Option<&Square> {
+        self.board.get(Self::resolve_index(coord))
+    }
+
+    pub fn iter(&self) -> BoardPathIter {
+        BoardPathIter {
+            board: self,
+            iter: Coord {x: 0, y: 0},
+            incr: (1,1),
+        }
+    }
+
+    pub fn iter_from(&self, from: Coord) -> BoardPathIter {
+        BoardPathIter {
+            board: self,
+            iter: from,
+            incr: (1,1)
+        }
+    }
+
+    pub fn iter_path(&self, from: Coord, incr: (isize, isize)) -> BoardPathIter {
+        BoardPathIter {
+            board: self,
+            iter: from,
+            incr,
+        }
+    }
+}
+
+struct BoardPathIter<'a> {
+    board: &'a Board,
+    iter: Coord,
+    incr: (isize, isize),
+}
+
+impl<'a> Iterator for BoardPathIter<'a> {
+    type Item = (Coord, &'a Square);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = (self.iter, self.board.get(self.iter)?);
+
+        self.iter.x += self.incr.0;
+        self.iter.y += self.incr.1;
+
+        Some(result)
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Coord {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Clone, Copy)]
+struct Path {
+    from: Coord,
+    to: Coord,
+}
+
+impl Path {
+    fn diff(&self) -> (isize, isize) {
+        (self.to.x as isize - self.from.x, self.to.y as isize - self.from.y)
+    }
+}
+
+// TODO: FEN, PGN, Game Impl
 fn main() {
     println!("Hello, world!");
 }
